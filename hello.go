e6cd6de617b1cabf51d280cac02742de6fe1361a
Marco Pla <marco.pla@gmail.com>
2024-02-10 23:32:53 -0300

    feat(log) : os.O_APPEND flag added to OpenFile() and WriteString() and strconv.FormatBool() applied

--- hello.go
+++ hello.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"os"
 	"reflect"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -17,8 +18,6 @@ const delay = 3
 func main() {
 
 	exibeIntroducao()
-
-	registraLog("site-falso", false)
 
 	for {
 		exibeMenu()
@@ -116,11 +115,11 @@ func leSitesDoArquivo() []string {
 
 func registraLog(site string, status bool) {
 
-	arquivo, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE, 0666)
+	arquivo, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 
 	if err != nil {
 		fmt.Println("Ocorreu o erro:", err)
 	}
-
-	fmt.Println(arquivo)
+	arquivo.WriteString(site + "- online: " + strconv.FormatBool(status) + "\n")
+	arquivo.Close()
 }
